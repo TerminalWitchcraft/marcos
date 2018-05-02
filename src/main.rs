288a323c31ca6ce979231b697108d117ef078fb7
Hitesh Paul <hiteshpaul94@gmail.com>
2018-05-02 15:19:34 +0530

    Added basic three column rendering

--- src/main.rs
+++ src/main.rs
@@ -18,16 +18,18 @@ use tui::layout::{Direction, Group, Rect, Size};
 use tui::style::{Color, Modifier, Style};
 
 
-struct App<'a>{
+struct App{
     size: Rect,
-    items: Vec<&'a str>
+    selected: usize,
+    item_len: usize
 }
 
-impl<'a> App<'a> {
-    pub fn new() -> App<'a> {
+impl App {
+    pub fn new() -> App {
         App {
             size: Rect::default(),
-            items: vec!["item1", "item2"],
+            selected: 0,
+            item_len: 0,
         }
     }
 }
@@ -61,7 +63,7 @@ fn main() {
     terminal.clear().unwrap();
     terminal.hide_cursor().unwrap();
     app.size = terminal.size().unwrap();
-    draw(&mut terminal, &app);
+    draw(&mut terminal, &mut app);
 
     loop {
         let size = terminal.size().unwrap();
@@ -75,39 +77,76 @@ fn main() {
                     terminal.clear().unwrap();
                     terminal.show_cursor().unwrap();
                     break;
-                }
+                },
+                event::Key::Up => if app.selected > 0 {
+                    app.selected -= 1;
+                } else {
+                    app.selected = app.item_len - 1;
+                },
+                event::Key::Char('k') => if app.selected > 0 {
+                    app.selected -= 1;
+                } else {
+                    app.selected = app.item_len - 1;
+                },
+                event::Key::Down => {
+                    app.selected += 1;
+                    if app.selected > app.item_len - 1 {
+                        app.selected = 0;
+                    }
+                },
+                event::Key::Char('j') => {
+                    app.selected += 1;
+                    if app.selected > app.item_len - 1 {
+                        app.selected = 0;
+                    }
+                },
                 _ => {}
             },
         }
-        draw(&mut terminal, &app);
+        draw(&mut terminal, &mut app);
     }
 
 }
 
-fn draw(t: &mut Terminal<MouseBackend>, app: &App) {
+fn draw(t: &mut Terminal<MouseBackend>, app: &mut App) {
+    let style = Style::default().fg(Color::White).bg(Color::Black);
+    let paths = read_dir("./").unwrap();
+    let current_list = paths.into_iter().map(|e| {
+        let dir = e.unwrap();
+        let p = dir.path();
+        p
+    }).collect::<Vec<_>>();
+    let current_list = current_list.iter().map(|e| {
+        match e.to_str() {
+            Some(data) => data,
+            None => "",
+        }
+    }).collect::<Vec<_>>();
+    app.item_len = current_list.len();
     Group::default()
         .direction(Direction::Horizontal)
         .sizes(&[Size::Percent(20), Size::Percent(40), Size::Percent(40)])
         .render(t, &app.size, |t, chunks| {
-            let style = Style::default().fg(Color::White).bg(Color::Black);
-            let paths = read_dir("./").unwrap();
-            let items = paths.map(|e| {
-                match e {
-                    Ok(entry) => entry.path(),
-                    _ => PathBuf::new(),
-                }
-            }).collect::<Vec<_>>();
             SelectableList::default()
                 .block(Block::default().title("Previous").borders(Borders::ALL))
-                .items(&app.items)
+                //.items(&app.items)
+                .items(&current_list)
+                .highlight_style(Style::default().fg(Color::Black).bg(Color::White))
+                .select(app.selected)
                 .render(t, &chunks[0]);
-            Block::default()
-                .title("Curret")
-                .borders(Borders::ALL)
+            SelectableList::default()
+                .block(Block::default().title("Previous").borders(Borders::ALL))
+                //.items(&app.items)
+                .items(&current_list)
+                .highlight_style(Style::default().fg(Color::Black).bg(Color::White))
+                .select(app.selected)
                 .render(t, &chunks[1]);
-            Block::default()
-                .title("Preview")
-                .borders(Borders::ALL)
+            SelectableList::default()
+                .block(Block::default().title("Previous").borders(Borders::ALL))
+                //.items(&app.items)
+                .items(&current_list)
+                .highlight_style(Style::default().fg(Color::Black).bg(Color::White))
+                .select(app.selected)
                 .render(t, &chunks[2]);
         });
     t.draw().unwrap();
