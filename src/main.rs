db521edc2275842583aa8eea9018b32e88775481
Hitesh Paul <hiteshpaul94@gmail.com>
2018-07-02 00:58:06 +0530

    Few modifications

--- src/main.rs
+++ src/main.rs
@@ -12,17 +12,21 @@ extern crate termion;
 use marcos::ui::tab::MyTab;
 // use marcos::events::input::InputThread;
 // use marcos::core::app::App;
-// use marcos::core;
+use marcos::core;
 
 use cursive::Cursive;
 use cursive::traits::Boxable;
 use cursive::theme::{Color, PaletteColor, Theme};
 use cursive::views::{Dialog, TextView, SelectView};
 use cursive::views::{LinearLayout, DummyView};
+use cursive::align::HAlign;
 use cursive::align;
+
+use termion::terminal_size;
 use std::fs::{self, DirEntry, File};
 use std::io::Read;
 use std::path::Path;
+
 
 fn file_picker<D>(directory: D) -> SelectView<DirEntry>
     where D: AsRef<Path>
@@ -62,124 +66,30 @@ fn load_contents(app: &mut Cursive, entry: &DirEntry) {
 
 
 fn main() {
+    core::log::setup_logger().unwrap();
 
     let default_tab = MyTab::default();
-    let mut file_view = SelectView::default();
+    info!("The terminal size is: {:?}", terminal_size().unwrap());
+    let mut current_view = SelectView::default();
+    let mut parent_view = SelectView::default();
     for item in default_tab.current.get_entries().iter() {
-        file_view.add_item(item.to_string(), 1);
+        current_view.add_item(item.to_string(), 1);
     }
-    // file_view.align(align::Align::new(align::HAlign.get_offset(5,5),
-    // align::VAlign.get_offset(5,5)));
+    for item in default_tab.parent.get_entries().iter() {
+        parent_view.add_item(item.to_string(), 1);
+    }
+    parent_view.set_selection(default_tab.get_parent_index());
+    parent_view.set_enabled(false);
     let mut siv = Cursive::default();
     let mut panes = LinearLayout::horizontal();
-    panes.add_child(file_view.full_width().full_height());
+    panes.add_child(parent_view.max_width(70).min_width(40).full_height());
+    panes.add_child(current_view.max_width(70).min_width(40).full_height());
     panes.add_child(DummyView);
-    panes.add_child(TextView::new("Contents"));
+    panes.add_child(TextView::new("Contents").full_width());
+    //info!("Current selction is: {:?}", current_view.selection());
 
-    // let mut layout = LinearLayout::vertical();
-    // layout.add_child(panes);
-    // layout.add_child(DummyView);
-    // layout.add_child(TextView::new("Status"));
     siv.add_layer(Dialog::around(panes).padding((0,0,0,0))); //.button("Quit", |a| a.quit()));
     siv.add_global_callback('q', |s| s.quit());
     siv.load_theme_file("assets/style.toml").unwrap();
     siv.run();
-    // //Set up logging
-    // core::log::setup_logger().unwrap();
-
-    // let backend = MouseBackend::new().unwrap();
-    // let mut terminal = Terminal::new(backend).unwrap();
-
-    // // for keysrokes threads
-
-    // let input_thread = InputThread::new();
-    // InputThread::spawn(input_thread.clone_tx());
-    // //input_thread.spawn(input_thread.clone_tx());
-    // 
-    // //Input
-
-    // let mut app = App::new();
-
-    // terminal.clear().unwrap();
-    // terminal.hide_cursor().unwrap();
-    // app.size = terminal.size().unwrap();
-    // //app.tabs.push(MyTab::new());
-    // app.add_tab(MyTab::default());
-    // draw(&mut terminal, &mut app);
-
-    // //the main loop for ui draw
-    // loop {
-    //     let size = terminal.size().unwrap();
-    //     if size != app.size {
-    //         info!("Change in terminal size");
-    //         terminal.resize(size).unwrap();
-    //         app.size = size;
-    //     }
-
-    //     let evt = input_thread.get_evt().unwrap();
-    //     match evt {
-    //             event::Key::Char('q') => {
-    //                 terminal.clear().unwrap();
-    //                 terminal.show_cursor().unwrap();
-    //                 break;
-    //             },
-    //             event::Key::Up => if app.selected > 0 {
-    //                 app.selected -= 1;
-    //             } else {
-    //                 app.selected = app.tabs[app.selected_tab].current.count - 1;
-    //             },
-    //             event::Key::Char('k') => if app.selected > 0 {
-    //                 app.selected -= 1;
-    //             } else {
-    //                 app.selected = app.tabs[app.selected_tab].current.count - 1;
-    //             },
-    //             event::Key::Down => {
-    //                 app.selected += 1;
-    //                 if app.selected > app.tabs[app.selected_tab].current.count - 1 {
-    //                     app.selected = 0;
-    //                 }
-    //             },
-    //             event::Key::Char('j') => {
-    //                 app.selected += 1;
-    //                 if app.selected > app.tabs[app.selected_tab].current.count - 1 {
-    //                     app.selected = 0;
-    //                 }
-    //             },
-    //             event::Key::Char(':') => {
-    //                 // Shows the command box
-    //                 app.show_command_box = true;
-    //                 // loop through keys until they press enter or esc
-    //                 terminal.show_cursor().unwrap();
-    //                 loop {
-    //                     let in_evt = input_thread.get_evt().unwrap();
-    //                     match in_evt {
-    //                         event::Key::Esc     => {
-    //                             terminal.hide_cursor().unwrap();
-    //                             app.show_command_box = false;
-    //                             break;
-    //                         },
-    //                         event::Key::Backspace => {
-    //                             app.command.pop();
-    //                         },
-    //                         event::Key::Char('\n') => {
-    //                             app.run_command();
-    //                             terminal.hide_cursor().unwrap();
-    //                             terminal.clear().unwrap();
-    //                             draw(&mut terminal, &mut app);
-    //                             app.show_command_box = false;
-    //                             break;
-    //                         },
-    //                         event::Key::Char(c)  => {
-    //                             app.command.push(c)
-    //                         },
-    //                         _   => {},
-    //                     }
-    //                 }
-    //             }
-    //             _ => {}
-
-    //     }
-    //     draw(&mut terminal, &mut app)
-    // }
-    // terminal.show_cursor().unwrap();
 }
