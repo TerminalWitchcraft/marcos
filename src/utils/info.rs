28b1af5e79e7d4925ef745a97ea2f435b6ca16b2
Hitesh Paul <hiteshpaul94@gmail.com>
2018-09-12 10:24:26 +0530

    Add module for info

---
+++ src/utils/info.rs
@@ -0,0 +1,28 @@
+//! Module which contains information related to system paths, files, folders, available space, etc
+
+use std::path::Path;
+use std::fmt::Display;
+
+use uname::uname;
+use users::get_current_username;
+use systemstat::{ System, Platform};
+
+/// Funtion to get user information as a String in the form: 
+/// `username @ host`
+pub fn user_info() -> String {
+    let user_name = get_current_username().unwrap_or("NA".to_string());
+    let user_data = uname().unwrap();
+    format!("{} @ {}", user_name, user_data.nodename)
+}
+
+/// Function to get mount point info as a std::String in the form:
+/// `_path available_ of _total_`
+pub fn disk_info<P: AsRef<Path> + Display>(path: P) -> String {
+    let sys = System::new();
+    let mount_info: String = match sys.mount_at(&path) {
+        Ok(mount)  => format!("{} {} available of {}", path, mount.avail, mount.total),
+        // TODO Make error string less verbose
+        Err(_e)      => String::from("Failed to read mount info!"),
+    };
+    mount_info
+}
